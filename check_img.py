d855f1cb38a1e2a3cc2fb897d470e2135754610e
georgytheboss <georgyjose007@gmail.com>
2016-09-14 18:18:44 +0530

    Photo link download complete

--- check_img.py
+++ check_img.py
@@ -2,9 +2,17 @@ import pickle
 
 a=pickle.load(open("images_found.p","rb"))
 
-key=raw_input("Enter the name of the car: ")
+'''key=raw_input("Enter the name of the car: ")
 
 print
 print key
 print a[key]
-print
+print 
+print len(a)'''
+
+for i in a:
+	print
+	print i
+	print a[i]
+	print
+	print "Links containing the photos of",len(a),"cars available"
